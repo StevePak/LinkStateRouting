1800aa91f9e725241f4179f60b1f27f638f3dc49
Pak, Steve <SPak@verisk.com>
2016-12-14 09:36:45 -0500

    Added LSP class.

---
+++ LinkStateRouting/LSP.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkStateRouting
+{
+    class LSP
+    {
+        public int SourceId { get; }
+        public int SequenceId { get; }
+        public int TTL { get; set; }
+        public List<int> ReachableNetwork { get; set; }
+
+        public LSP(int source, int sequence, List<int> reachableNet)
+        {
+            SourceId = source;
+            SequenceId = sequence;
+            TTL = 10;
+            ReachableNetwork = reachableNet;
+        }
+    }
+}
