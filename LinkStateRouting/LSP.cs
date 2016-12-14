bfdd40df379ddb0b787aa0f462e8629ed8d77490
Pak, Steve <SPak@verisk.com>
2016-12-14 11:55:22 -0500

    Changed LSP to also hold the cost of the Route. Implemented discard case of the receive packet command of the router.

--- LinkStateRouting/LSP.cs
+++ LinkStateRouting/LSP.cs
@@ -11,14 +11,20 @@ namespace LinkStateRouting
         public int SourceId { get; }
         public int SequenceId { get; }
         public int TTL { get; set; }
-        public List<int> ReachableNetwork { get; set; }
+        public List<RouterCost> ReachableNetwork { get; set; }
 
-        public LSP(int source, int sequence, List<int> reachableNet)
+        public LSP(int source, int sequence, List<RouterCost> reachableNet)
         {
             SourceId = source;
             SequenceId = sequence;
             TTL = 10;
             ReachableNetwork = reachableNet;
+        }
+
+        public class RouterCost
+        {
+            public int RouterId { get; set; }
+            public int TotalCost { get; set; }
         }
     }
 }
--- LinkStateRouting/Router.cs
+++ LinkStateRouting/Router.cs
@@ -12,19 +12,9 @@ namespace LinkStateRouting
         public string networkName { get; set; }
         public int networkCost { get; set; }
 
-        public List<Router> Connections { get; set; }
+        public List<Connection> Connections { get; set; }
 
         public Dictionary<int, RoutingInfo> RoutingTable { get; set; }
-
-        public void originatePackage()
-        {
-
-        }
-
-        public void receivePackage(LSP pack)
-        {
-
-        }
 
         public class RoutingInfo
         {
@@ -32,5 +22,33 @@ namespace LinkStateRouting
             public int Cost { get; set; }
             public int Link { get; set; }
         }
+
+        public class Connection
+        {
+            public Router Router;
+            public int RecentSequence;
+            public int TickCount;
+        }
+
+        public void OriginatePackage()
+        {
+
+        }
+
+        public void ReceivePackage(LSP pack)
+        {
+            pack.TTL--;
+            Connection c = Connections.Where(x => x.Router.routerId == pack.SourceId).Single();
+            if (pack.TTL == 0 || c.RecentSequence >= pack.SequenceId)
+            {
+                return;
+            }
+        }
+
+        public void ConstructRoutingTable()
+        {
+           
+        }
+
     }
 }
